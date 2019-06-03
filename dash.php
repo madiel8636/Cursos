a44cae0f3af933ecda692a642a3b86dc58e4af8c
madiel8636 <maria.dmoreno.ag@gmail.com>
2019-06-02 19:08:22 -0500

    Update dash.php

--- dash.php
+++ dash.php
@@ -1,17 +1,7 @@
 <?php
   session_start();
   require 'include/conn.php';
-  if (isset($_SESSION['user_id'])) {
-    $id = $_SESSION['user_id'];
-    $result = mysqli_query($conn, 'SELECT * FROM usuarios WHERE id = '.$id.'');
-    $results = mysqli_fetch_assoc($result);
-    $user = null;
-    if (count($results) > 0) {
-      $user = $results;
-    }
-  } else {
-    header('Location: /cursos');
-  }
+  require 'include/session.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,34 +14,27 @@
             <div class="nav-wrapper light-blue darken-2">
                 <a href="#!" class="brand-logo center">Cursos</a>
                 <a href="#" data-target="mobile-demo" class="sidenav-trigger right"><i class="fas fa-bars"></i></a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    
-                    <?php if($user['tipo'] === "Administrador"): ?>
-                    <li><a href="adminarea.php">Admin</a></li>
-                    <?php endif; ?>
-                    <?php if($user['tipo'] === "Profesor" || $user['tipo'] === "Administrador"): ?>
-                    <li><a href="profesores.php">Profesores</a></li>
-                    <?php endif; ?>
+                <ul id="nav-mobile" class="right hide-on-med-and-down"> 
                     <li><a href="logout.php">Salir</a></li>
                 </ul>
             </div>
         </nav>
         <ul class="sidenav" id="mobile-demo">
-            <?php if($user['tipo'] === "Administrador"): ?>
-            <li><a href="adminarea.php">Admin</a></li>
-            <?php endif; ?>
-            <?php if($user['tipo'] === "Profesor" || $user['tipo'] === "Administrador"): ?>
-                    <li><a href="profesores.php">Profesores</a></li>
-                    <?php endif; ?>
             <li><a href="logout.php">Salir</a></li>
         </ul>
     </header>
     <main>
         <div class="container">
             <div class="row">
-                <div class="col">
-
-            </div>
+                <div class="col s12 m9">
+                    <div class="card">
+                        <div class="card-content">
+                            <?php 
+                            include_once 'include/table.php';
+                            ?>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </main>

11a2604784e45e5db71d9eb3378cbfdf55988f23
madiel8636 <maria.dmoreno.ag@gmail.com>
2019-06-02 21:47:05 -0500

    Create table.php

---
+++ include/table.php
@@ -0,0 +1,14 @@
+<table class="responsive-table">
+    <thead>
+        <tr>
+            <th></td>
+            <th></td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
